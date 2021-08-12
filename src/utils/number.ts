bf8025501ca1ba9ed5d236d67f18f03134146688
Alexander Widua <a.widua@gmail.com>
2021-08-12 14:50:32 +0200

    style: add comment

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,3 +1,6 @@
+/**
+ * Rounds number to show trailing decimals.
+ */
 export function showDecimals(number: number, decimals: number): string {
 	return (Math.round(number * 100) / 100).toFixed(decimals)
 }
