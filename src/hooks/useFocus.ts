c44777bb9c4d34e79d3a9b552faf92f73b26aa65
Alexander Widua <a.widua@gmail.com>
2021-08-13 20:01:19 +0200

    feat: add useFocus hook

---
+++ src/hooks/useFocus.ts
@@ -0,0 +1,27 @@
+import { useEffect } from 'preact/hooks'
+
+const FOCUS_DATA_ATTRIBUTE_NAME = 'data-focus'
+
+export type InitialFocus = {
+	[FOCUS_DATA_ATTRIBUTE_NAME]: true
+}
+
+export function useFocus(shouldFocus: boolean): InitialFocus {
+	useEffect(
+		function (): void {
+			const focusableElements = document.querySelectorAll<HTMLElement>(
+				`[${FOCUS_DATA_ATTRIBUTE_NAME}]`
+			)
+			if (focusableElements.length === 0) {
+				throw new Error(
+					`No element with attribute \`${FOCUS_DATA_ATTRIBUTE_NAME}\``
+				)
+			}
+			if (shouldFocus) focusableElements[0].focus()
+		},
+		[shouldFocus]
+	)
+	return {
+		[FOCUS_DATA_ATTRIBUTE_NAME]: true
+	}
+}
