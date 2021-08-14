1e2bb98c2c6ef980e49d1430db53242b46f9c8e0
Alexander Widua <a.widua@gmail.com>
2021-08-14 20:21:55 +0200

    refactor: move preset states from child to parent component

--- src/components/PresetMenu/index.tsx
+++ src/components/PresetMenu/index.tsx
@@ -1,23 +1,13 @@
 import { h, JSX } from 'preact'
-import {
-	useState,
-	useMemo,
-	useRef,
-	useEffect,
-	useImperativeHandle
-} from 'preact/hooks'
+import { useState, useMemo, useRef, useEffect } from 'preact/hooks'
 import {
 	Dropdown,
 	DropdownOption,
 	useMouseDownOutside
 } from '@create-figma-plugin/ui'
-import { on, once, emit } from '@create-figma-plugin/utilities'
 import { useFocus } from '../../hooks/useFocus'
 
 const PRESET_MENU_PLACEHOLDER: string = 'Choose preset...'
-const NO_PRESETS_AVAILABLE_OPTION: Array<DropdownOption> = [
-	{ header: 'No userPresets.' }
-]
 const MENU_OPTIONS: Array<DropdownOption> = [
 	{ separator: true },
 	{ children: 'Add current as preset', value: 'ADD_PRESET' },
@@ -27,16 +17,14 @@ const MENU_OPTIONS: Array<DropdownOption> = [
 	}
 ]
 
-const PresetMenu = ({ matrix, onChange }: any) => {
-	/**
-	 * States
-	 */
-	const [userPresets, setUserPresets] = useState<any>(
-		NO_PRESETS_AVAILABLE_OPTION
-	)
+const PresetMenu = ({
+	value,
+	isManagingPresets,
+	userPresets,
+	onValueChange
+}: any) => {
 	const menuOptions = MENU_OPTIONS
 
-	const [selectedOption, setSelectedOption] = useState<string | null>(null)
 	const defaultOptions = useMemo<Array<DropdownOption>>(() => {
 		return [...userPresets, ...menuOptions]
 	}, [userPresets])
@@ -50,128 +38,25 @@ const PresetMenu = ({ matrix, onChange }: any) => {
 		]
 	}, [userPresets])
 
-	const [showManagePresetOptions, setShowManagePresetOptions] =
-		useState<boolean>(false)
-
-	const ref = useRef<HTMLDivElement>(null)
-
-	/**
-	 * Hooks
-	 */
-	useEffect(() => {
-		on('EMIT_PRESETS_TO_UI', (storedPresets) => {
-			if (storedPresets.length) {
-				setUserPresets([...storedPresets])
-			}
-		})
-		on('RESPOND_TO_PRESETS_UPDATE', handleResponseFromPlugin)
-	}, [])
-
-	useEffect(() => {
-		if (selectedOption) {
-			const temp = [...userPresets]
-			const presetMatrix = temp.find(
-				(el) => el.value === selectedOption
-			).matrix
-			onChange(presetMatrix)
-		}
-	}, [selectedOption])
-
-	// useEffect(() => {
-	// 	setSelectedOption(null)
-	// }, [matrix])
-
-	/**
-	 * Input event handlers
-	 */
 	function handlePresetInput(e: JSX.TargetedEvent<HTMLInputElement>): void {
 		const value = e.currentTarget.value
 
-		if (value === 'ADD_PRESET') {
-			// TODO: generate unique custom elements
-			const temp = [...userPresets]
-			const newPreset: any = {
-				children: 'Custom_1',
-				value: 'CUSTOM_1',
-				matrix: [...matrix]
-			}
-			if (temp.some((el) => el.header)) {
-				emitUserPresetUpdate([newPreset])
-			} else {
-				emitUserPresetUpdate([...temp, newPreset])
-			}
-		} else if (value === 'MANAGE_PRESETS') {
-			setShowManagePresetOptions(true)
-			setSelectedOption(null)
-		} else {
-			setSelectedOption(value)
-		}
+		onValueChange(value)
 	}
 
-	function handleManagePresets(e: JSX.TargetedEvent<HTMLInputElement>): void {
-		const value = e.currentTarget.value
-		if (value !== 'RESET_DEFAULT') {
-			const temp = [...userPresets]
-			const updatedPresets = temp.filter((e) => e.value !== value)
-			if (!updatedPresets.length) {
-				emitUserPresetUpdate([])
-			} else {
-				emitUserPresetUpdate([...updatedPresets])
-			}
-		} else {
-			// TODO: Initiate reset here...
-		}
-		setShowManagePresetOptions(false)
-	}
-
-	/**
-	 * Handle emits to plugin/store updates
-	 */
-	function emitUserPresetUpdate(userPresets: Array<DropdownOption>): void {
-		emit('EMIT_PRESETS_TO_PLUGIN', userPresets)
-	}
-
-	// TODO: Type.
-	function handleResponseFromPlugin(response: Array<any> | undefined): void {
-		if (response) {
-			if (!response.length) {
-				setUserPresets(NO_PRESETS_AVAILABLE_OPTION)
-			} else {
-				// if presets has been added, select it
-				if (response.length > userPresets.length) {
-					setSelectedOption(response[response.length - 1].value)
-				}
-				setUserPresets([...response])
-			}
-		}
-	}
-
-	/**
-	 * Misc
-	 */
-	useMouseDownOutside({
-		onMouseDownOutside: () => setShowManagePresetOptions(false),
-		ref
-	})
+	//useMouseDownOutside({
+	//	onMouseDownOutside: () => setShowManagePresetOptions(false),
+	//	ref
+	//})
 
 	return (
-		<div ref={ref}>
-			<Dropdown
-				{...useFocus(showManagePresetOptions)}
-				value={showManagePresetOptions ? null : selectedOption}
-				onChange={
-					showManagePresetOptions
-						? handleManagePresets
-						: handlePresetInput
-				}
-				options={
-					showManagePresetOptions
-						? managePresetsOptions
-						: defaultOptions
-				}
-				placeholder={PRESET_MENU_PLACEHOLDER}
-			/>
-		</div>
+		<Dropdown
+			{...useFocus(isManagingPresets)}
+			value={isManagingPresets ? null : value}
+			onChange={handlePresetInput}
+			options={isManagingPresets ? managePresetsOptions : defaultOptions}
+			placeholder={PRESET_MENU_PLACEHOLDER}
+		/>
 	)
 }
 
--- src/ui.tsx
+++ src/ui.tsx
@@ -16,10 +16,20 @@ import { showDecimals } from './utils/number'
 
 import { PresetMenu, Editor } from './components'
 
+const NO_PRESETS_AVAILABLE_OPTION: Array<DropdownOption> = [
+	{ header: 'No userPresets.' }
+]
+
 const Plugin = () => {
 	// ui-exposed states
 	const [easingType, setEasingType] = useState<string>('CURVE')
 
+	// UI component should control preset menu state
+	const [selectedPreset, setSelectedPreset] = useState<string | null>(null)
+	const [userPresets, setUserPresets] = useState<any>(
+		NO_PRESETS_AVAILABLE_OPTION
+	)
+	const [isManagingPresets, setIsManagingPresets] = useState(false)
 	const [matrix, setMatrix] = useState<Matrix>([
 		[0.42, 0.0],
 		[0.58, 1.0]
@@ -57,6 +67,14 @@ const Plugin = () => {
 		on('UPDATE_SELECTION_STATE', (selectionState) => {
 			setSelectionState(selectionState)
 		})
+
+		// Editor refactor
+		on('EMIT_PRESETS_TO_UI', (storedPresets) => {
+			if (storedPresets.length) {
+				setUserPresets([...storedPresets])
+			}
+		})
+		on('RESPOND_TO_PRESETS_UPDATE', handleResponseFromPlugin)
 	}, [])
 
 	useEffect(() => {
@@ -99,10 +117,6 @@ const Plugin = () => {
 		}
 	}
 
-	function handlePresetChange(matrix: Matrix) {
-		setMatrix(matrix)
-	}
-
 	/**
 	 * Debounce message emit
 	 */
@@ -113,6 +127,71 @@ const Plugin = () => {
 		debounce((data) => emit('UPDATE_FROM_UI', data), debounceWaitTime),
 		[]
 	)
+
+	/**
+	 * Refactor editor
+	 * TODO: Organize funcs
+	 */
+	function handlePresetMenuChange(value: string) {
+		if (!isManagingPresets) {
+			if (value === 'ADD_PRESET') {
+				// TODO: generate unique custom elements
+				let data
+				const temp = [...userPresets]
+				const newPreset: any = {
+					children: 'Custom_1',
+					value: 'CUSTOM_1',
+					matrix: [...matrix]
+				}
+				if ([...userPresets].some((el) => el.header)) {
+					data = [newPreset]
+				} else {
+					data = [...temp, newPreset]
+				}
+				emitUserPresetUpdate(data)
+			} else if (value === 'MANAGE_PRESETS') {
+				setIsManagingPresets(true)
+				setSelectedPreset(null)
+			} else {
+				setSelectedPreset(value)
+			}
+		} else {
+			if (value !== 'RESET_DEFAULT') {
+				let data: any
+				const updatedPresets = [...userPresets].filter(
+					(e) => e.value !== value
+				)
+				if (!updatedPresets.length) {
+					data = []
+				} else {
+					data = [...updatedPresets]
+				}
+				emitUserPresetUpdate(data)
+			} else {
+				// TODO: Initiate reset here...
+			}
+			setIsManagingPresets(false)
+		}
+	}
+
+	function emitUserPresetUpdate(userPresets: Array<DropdownOption>): void {
+		emit('EMIT_PRESETS_TO_PLUGIN', userPresets)
+	}
+
+	// TODO: Type.
+	function handleResponseFromPlugin(response: Array<any> | undefined): void {
+		if (response) {
+			if (!response.length) {
+				setUserPresets(NO_PRESETS_AVAILABLE_OPTION)
+			} else {
+				// if presets has been added, select it
+				if (response.length > userPresets.length) {
+					setSelectedPreset(response[response.length - 1].value)
+				}
+				setUserPresets([...response])
+			}
+		}
+	}
 
 	return (
 		<Container>
@@ -128,7 +207,12 @@ const Plugin = () => {
 					options={presetOptions}
 					placeholder="Custom"
 				/> */}
-				<PresetMenu matrix={matrix} onChange={handlePresetChange} />
+				<PresetMenu
+					value={selectedPreset}
+					userPresets={userPresets}
+					isManagingPresets={isManagingPresets}
+					onValueChange={handlePresetMenuChange}
+				/>
 			</Columns>
 			<Editor
 				easingType={easingType}
