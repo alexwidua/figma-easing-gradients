0d49c15d2babb7fe62e8a097cc45f89996f92ba3
Alexander Widua <a.widua@gmail.com>
2021-08-17 11:15:19 +0200

    add more typings

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,10 @@
+// import {
+// 	DropdownOptionHeader,
+// 	DropdownOptionValue,
+// 	DropdownOptionSeparator
+// } from '@create-figma-plugin/ui'
+// import { ComponentChildren } from 'preact'
+
 /**
  * Data that is applied to the plugin's UI window.
  */
@@ -17,6 +24,20 @@ type EasingOptions = {
 	skip: string
 }
 
+type EditorChange = {
+	type: EasingType
+	thumb?: { index: EditorInputIndex; vector: Array<number> }
+	steps?: number
+}
+
+type EditorInputIndex = -1 | 0 | 1 | 2
+
+// type PresetOption = {
+// 	children: import('preact').ComponentChildren
+// 	value: string
+// 	matrix: Matrix
+// }
+
 type SelectionState =
 	| 'VALID'
 	| 'INVALID_TYPE'
@@ -24,3 +45,34 @@ type SelectionState =
 	| 'NO_GRADIENT_FILL'
 	| 'EMPTY'
 type SelectionStateMap = { [type in SelectionState]: string }
+
+type ErrorKey = 'PRESET_INPUT_TOO_MANY_CHARS'
+type ErrorMap = { [type in ErrorKey]: string }
+
+//
+
+type PresetOption =
+	| PresetOptionHeader
+	| PresetOptionValue
+	| PresetOptionSeparator
+
+type PresetOptionHeader = {
+	header: string
+}
+type PresetOptionValue = {
+	children?: string
+	value: PresetOptionKey
+	matrix?: Matrix
+}
+
+type PresetOptionSeparator = {
+	separator: boolean
+}
+
+type PresetOptionKey =
+	| 'ADD_PRESET'
+	| 'MANAGE_PRESETS'
+	| 'RESET_DEFAULT'
+	| GeneratedCustomPresetKey
+
+type GeneratedCustomPresetKey = `CUSTOM_${string}`
