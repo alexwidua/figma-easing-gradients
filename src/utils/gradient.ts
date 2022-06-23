ffffa5876f9c69eb447d75953b19f7a39bb29818
Andrew Pouliot <andrew@darknoon.com>
2022-06-23 15:22:40 -0400

    prettier again

--- src/utils/gradient.ts
+++ src/utils/gradient.ts
@@ -13,9 +13,9 @@ import { EasingType, EasingOptions } from '../main'
  * @returns false if fill isn't a gradient fill, ex. SolidPaint or ImagePaint
  */
 export function isGradientFillWithMultipleStops(
-    fill: Paint
+	fill: Paint
 ): fill is GradientPaint {
-    return 'gradientStops' in fill && fill?.gradientStops?.length > 1
+	return 'gradientStops' in fill && fill?.gradientStops?.length > 1
 }
 
 /**
@@ -23,52 +23,52 @@ export function isGradientFillWithMultipleStops(
  * @returns Returns an array of color stops which represents the eased color gradient.
  */
 export function interpolateColorStops(
-    fill: GradientPaint,
-    options: EasingOptions
+	fill: GradientPaint,
+	options: EasingOptions
 ): ColorStop[] {
-    const { type, matrix, steps, skip } = options
-    const { gradientStops } = fill
+	const { type, matrix, steps, skip } = options
+	const { gradientStops } = fill
 
-    console.log('interpolating ', { options, gradientStops })
+	console.log('interpolating ', { options, gradientStops })
 
-    // How many color stops are used to interpolate between first and last stop
-    // TODO: Should this be an user-facing option?
-    const granularity = 15
+	// How many color stops are used to interpolate between first and last stop
+	// TODO: Should this be an user-facing option?
+	const granularity = 15
 
-    let stops: ColorStop[] = []
-    for (let i = 0; i < gradientStops.length - 1; i++) {
-        const start = gradientStops[i]
-        const end = gradientStops[i + 1]
+	let stops: ColorStop[] = []
+	for (let i = 0; i < gradientStops.length - 1; i++) {
+		const start = gradientStops[i]
+		const end = gradientStops[i + 1]
 
-        const coordinates =
-            type == 'CURVE'
-                ? easingCoordinates(
-                      `cubic-bezier(
+		const coordinates =
+			type == 'CURVE'
+				? easingCoordinates(
+						`cubic-bezier(
         				${matrix[0][0]},
         				${matrix[0][1]},
         				${matrix[1][0]},
         				${matrix[1][1]})`,
-                      granularity
-                  )
-                : easingCoordinates(`steps(${steps}, ${skip})`)
+						granularity
+				  )
+				: easingCoordinates(`steps(${steps}, ${skip})`)
 
-        // if we're not at the end, drop the last coordinate so we don't add redundant stops
-        if (i < gradientStops.length - 2) {
-            coordinates.pop()
-        }
+		// if we're not at the end, drop the last coordinate so we don't add redundant stops
+		if (i < gradientStops.length - 2) {
+			coordinates.pop()
+		}
 
-        stops = stops.concat(
-            coordinates.map((t) => {
-                const [r, g, b, a] = chroma
-                    .mix(gl(start), gl(end), t.y, 'rgb')
-                    .gl()
-                return {
-                    color: { r, g, b, a },
-                    position:
-                        start.position + t.x * (end.position - start.position),
-                }
-            })
-        )
-    }
-    return stops
+		stops = stops.concat(
+			coordinates.map((t) => {
+				const [r, g, b, a] = chroma
+					.mix(gl(start), gl(end), t.y, 'rgb')
+					.gl()
+				return {
+					color: { r, g, b, a },
+					position:
+						start.position + t.x * (end.position - start.position),
+				}
+			})
+		)
+	}
+	return stops
 }
